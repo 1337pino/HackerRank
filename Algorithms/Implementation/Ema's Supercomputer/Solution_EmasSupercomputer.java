4d4a9a330539e3e8e2b64bedcab401499cc97fc7
l337pino <1337pino@gmail.com>
2021-01-27 18:00:58 -0800

    Ema's Supercomputer Java solution

---
+++ Algorithms/Implementation/Ema's Supercomputer/Solution_EmasSupercomputer.java
@@ -0,0 +1,146 @@
+/** Ema's Supercomputer
+ * https://www.hackerrank.com/challenges/two-pluses/problem
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Solution_EmasSupercomputer {
+
+    // Complete the twoPluses function below.
+    static int twoPluses(char[][] grid) {
+        HashMap<Integer, List<Integer>> plusDatabase = new HashMap<Integer, List<Integer>>();
+        
+        int maxSize, maxRow = grid.length, maxCol = grid[0].length, biggestroduct = 1;
+        
+        // Find the theoretically max size a plus can be based on the size of the board
+        if (grid.length < grid[0].length) {
+            maxSize = maxRow;
+        } else {
+            maxSize = maxCol;
+        }
+        
+        // The max size needs to be odd per definition of the pluses
+        if (maxSize % 2 != 1) maxSize--;
+        
+        /* Search for pluses starting with the biggest possible sizes first and stopping
+            once the two biggest pluses are found */
+        for (int i = maxSize; i > 0; i -= 2) {
+            int armLength = i / 2;
+            List<Integer> validLocations = new ArrayList<Integer>();
+            
+            // Search the grid for valid locations for a plus of the given arm length
+            for (int j = armLength; j < maxRow - armLength; j++) {
+                for (int k = armLength; k < maxCol - armLength; k++) {
+                    if (verifyPlus(grid, armLength, j, k)) {
+                        validLocations.add((maxCol * j) + k);
+                    }
+                }
+            }
+            
+            // Update the database with the valid locations for the current-size plus
+            plusDatabase.put(armLength, validLocations);
+            
+            for (int j = maxSize / 2; j >= armLength; j--) {
+                List<Integer> locationsForPlusSizeJ = plusDatabase.get(j);
+
+                for (Integer plus : validLocations) {
+                    for (Integer otherPlus : locationsForPlusSizeJ) {
+                        // If the pluses don't overlap, update the largest product value if needed
+                        if (verifyNoInterceptions(plus, armLength, otherPlus, j, maxCol)) {
+                            int plusProduct = ((4 * armLength) + 1) * ((4 * j) + 1);
+
+                            if (plusProduct > biggestroduct) biggestroduct = plusProduct;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return biggestroduct;
+    }
+    
+    public static boolean verifyNoInterceptions(int plusA, int armlengthA, int plusB, int armlengthB, int gridWidth) {
+        List<Integer> plusACells = new ArrayList<Integer>();
+        plusACells.add(plusA);
+        fillPlusList(plusACells, armlengthA, plusA, gridWidth);
+        
+        List<Integer> plusBCells = new ArrayList<Integer>();
+        plusBCells.add(plusB);
+        fillPlusList(plusBCells, armlengthB, plusB, gridWidth);
+        
+        // Check for shared cells
+        for (Integer cell : plusACells) {
+            if (plusBCells.contains(cell)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private static void fillPlusList(List<Integer> plusList, int armLength, int plusCenter, int gridWidth) {
+        for (int i = armLength; i > 0; i--) {
+            // Upwards
+            plusList.add(plusCenter - (i * gridWidth));
+            
+            // Downwards
+            plusList.add(plusCenter + (i * gridWidth));
+            
+            // Left
+            plusList.add(plusCenter - i);
+            
+            // Right
+            plusList.add(plusCenter + i);
+        }
+    }
+    
+    public static boolean verifyPlus(char[][] grid, int armLength, int row, int col) {
+        // Verify vertical arms
+        for (int i = row - armLength; i <= row + armLength; i++) {
+            if (grid[i][col] == 'B') {
+                return false;
+            }
+        }
+        
+        // Verify horizontal arms
+        for (int i = col - armLength; i <= col + armLength; i++) {
+            if (grid[row][i] == 'B') {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+
+        char[][] grid = new char[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            String gridItem = scanner.nextLine();
+            
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = gridItem.charAt(j);
+            }
+        }
+
+        int result = twoPluses(grid);
+        System.out.println(result);
+
+        // bufferedWriter.write(String.valueOf(result));
+        // bufferedWriter.newLine();
+
+        // bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
